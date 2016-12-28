2c33895f69484ac58d65035e3edaabf6781d6dc7
JakovT4 <jakovtiefenbach@jakovs-macbook-air.local>
2016-12-28 13:41:30 +0100

    Initial Commit

---
+++ Testingmyskills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testingmyskills
+//
+//  Created by Jakov Tiefenbach on 28/12/2016.
+//  Copyright © 2016 Jakov Tiefenbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
